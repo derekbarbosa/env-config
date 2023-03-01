81bafa937c8bd15e0fbe8d7522b61622a6a27bbe
Derek <55967015+derekbarbosa@users.noreply.github.com>
2023-03-01 16:10:07 -0500

    Update fedora-server-setup.sh

--- fedora-server-setup.sh
+++ fedora-server-setup.sh
@@ -2,4 +2,4 @@
 
 ## Script To Setup Fedora Remotes
 
-sudo dnf install wget vim git fedpkg fedora-packager rpmdevtools ncurses-devel pesign grubby kernel-devel kernel-headers dwarves
+sudo dnf install wget vim git fedpkg fedora-packager rpmdevtools ncurses-devel pesign grubby kernel-devel kernel-headers dwarves bc clang lld 
