73b04e50e6e84007c40531b10e798d15fc9ab3f6
Derek <55967015+derekbarbosa@users.noreply.github.com>
2023-03-02 09:25:02 -0500

    Update fedora-server-setup.sh

--- fedora-server-setup.sh
+++ fedora-server-setup.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-## Script To Setup Fedora Remotes
+## Script To Setup Fedora Remotes for devel
 
 sudo dnf install wget vim git fedpkg fedora-packager rpmdevtools ncurses-devel pesign grubby kernel-devel kernel-headers dwarves bc clang lld 
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+source "$HOME/.cargo/env"
+
+rustup component add rust-src
+
+cargo install --locked --version $(scripts/min-tool-version.sh bindgen) bindgen
+
+
+mkdir ~/workspace
+
+git clone https://www.github.com/torvalds/linux ~/workspace/
