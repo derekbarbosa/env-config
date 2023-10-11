3648c234599182fd0b4c5c89f61daeee03202ae5
Derek Barbosa <derekasobrab@gmail.com>
2023-10-11 15:19:03 -0400

    script panic vars

--- setup.sh
+++ setup.sh
@@ -8,6 +8,13 @@ sudo dnf install -y "${REQUIRED_PKG[@]}" && echo "Success..."
 
 sudo dnf group install -y --with-optional "C Development Tools and Libraries"
 
+sysctl -w kernel.panic_on_io_nmi=1 >> /etc/sysctl.conf
+sysctl -w kernel.panic_on_unrecovered_nmi=1 >> /etc/sysctl.conf
+sysctl -w kernel.unknown_nmi_panic=1 >> /etc/sysctl.conf
+sysctl -w kernel.panic=1 >> /etc/sysctl.conf
+
+sysctl -p
+
 git clone https://git.kernel.org/pub/scm/linux/kernel/git/rt/linux-rt-devel.git 
 
 git clone git://git.kernel.org/pub/scm/linux/kernel/git/rt/linux-stable-rt.git
